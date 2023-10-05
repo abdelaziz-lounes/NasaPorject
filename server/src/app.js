8aff01524b75abc94f63385969ae887dd57b22c2
abdelaziz-lounes <abdelazizlounes2@gmail.com>
2023-10-05 08:12:37 +0100

    logging requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const path= require("path")
 const cors= require('cors');
+const express = require('express');
+const path= require("path");
+const morgan= require("morgan");
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app= express();
@@ -8,6 +9,7 @@ const app= express();
 app.use(cors(
     {origin:'http://localhost:3000',}
 ));
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,'..','public')));
 app.use(express.json());
 app.use(planetsRouter)
