69d146d285fd4ad7da16cf4710685bb47419af6c
abdelaziz_lounes <abdelazizlounes2@gmail.com>
2023-10-10 23:32:20 +0100

    post lunche1

--- server/src/models/lunches.model.js
+++ server/src/models/lunches.model.js
@@ -1,12 +1,14 @@
 const lunches= new Map();
 
+let latestFlightNumber= 100;
+
 const lunch= {
     FlightNumber:100,
     mission: 'Kepler Exploration',
     rocket: 'kepler E1',
     launchDate: new Date('01/12/2024'),
     destination: 'kepler-200 b',
-    customer: ['echouroq','djazairia'],
+    customer: ['AAF','Djazairia'],
     upcoming: true,
     success: true,
 
@@ -16,6 +18,16 @@ lunches.set(lunch.FlightNumber, lunch);
 
 function getAllLunches(){
     return Array.from(lunches.values());
+}
+
+function addNewLunch(lunch){
+    latestFlightNumber++;
+    lunches.set(latestFlightNumber, Object.assign(lunch, {
+        FlightNumber: latestFlightNumber,
+        customer: ['AAF','Djazairaia'],
+        upcoming: true,
+        success: true,
+    }));
 }
 
 module.exports={
