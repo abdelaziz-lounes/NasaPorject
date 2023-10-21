97c19d0db3b160a121363ef587086f4a3ba0a40a
abdelaziz_lounes <abdelazizlounes2@gmail.com>
2023-10-21 02:39:02 +0100

    testing endpoints with supertest Post

--- server/src/routes/lunches/launches.test.js
+++ server/src/routes/lunches/launches.test.js
@@ -9,8 +9,35 @@ describe("Test GET /launches", () => {
       .expect(200);
   });
 });
-describe("Test POST /launches", () => {
-  test("it should respond with 200 success", () => {});
+describe("Test POST /launch", () => {
+
+  test("it should respond with 201 created", async() => {
+    
+    const launchData = {
+      mission: "DZ explore",
+      rocket: "DZ E1",
+      launchDate: new Date("01/12/2025"),
+      target: "kepler-200 b",
+    };
+
+    const launchDataWithoutDate = {
+      mission: "DZ explore",
+      rocket: "DZ E1",
+      target: "kepler-200 b",
+    };
+
+    const response= await request(app)
+        .post("/launches")
+        .send(launchData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+
+    const requestDate=new Date(launchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    
+    expect(requestDate).toBe(requestDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
 
   test("it should check missing property", () => {});
 
