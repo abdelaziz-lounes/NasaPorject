6e735df4f8f2854f00f5666aaffd35ac508bfca6
abdelaziz_lounes <abdelazizlounes2@gmail.com>
2023-10-23 18:33:32 +0100

    improvig performance with pm2

--- server/src/routes/lunches/launches.test.js
+++ server/src/routes/lunches/launches.test.js
@@ -10,36 +10,61 @@ describe("Test GET /launches", () => {
   });
 });
 describe("Test POST /launch", () => {
+  const launchData = {
+    mission: "DZ explore",
+    rocket: "DZ E1",
+    launchDate: new Date("01/12/2025"),
+    target: "kepler-200 b",
+  };
 
-  test("it should respond with 201 created", async() => {
-    
-    const launchData = {
-      mission: "DZ explore",
-      rocket: "DZ E1",
-      launchDate: new Date("01/12/2025"),
-      target: "kepler-200 b",
-    };
+  const invalidDateLaunchData = {
+    mission: "DZ explore",
+    rocket: "DZ E1",
+    launchDate: "caw",
+    target: "kepler-200 b",
+  };
 
-    const launchDataWithoutDate = {
-      mission: "DZ explore",
-      rocket: "DZ E1",
-      target: "kepler-200 b",
-    };
+  const launchDataWithoutDate = {
+    mission: "DZ explore",
+    rocket: "DZ E1",
+    target: "kepler-200 b",
+  };
 
-    const response= await request(app)
-        .post("/launches")
-        .send(launchData)
-        .expect("Content-Type", /json/)
-        .expect(201);
+  test("it should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
 
-    const requestDate=new Date(launchData.launchDate).valueOf();
+    const requestDate = new Date(launchData.launchDate).valueOf();
     const responseDate = new Date(response.body.launchDate).valueOf();
-    
-    expect(requestDate).toBe(requestDate);
+
+    expect(requestDate).toBe(responseDate);
     expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
-  test("it should check missing property", () => {});
+  test("it should check missing property", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
 
-  test("it should catch invalid dates", () => {});
+    expect(response.body).toStrictEqual({
+      error: "missing required launch property",
+    });
+  });
+
+  test("it should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(invalidDateLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "invalid date format",
+    });
+  });
 });
--- server/src/routes/lunches/lunches.controller.js
+++ server/src/routes/lunches/lunches.controller.js
@@ -1,50 +1,57 @@
-const {getAllLaunches,addNewLaunch,existLaunchWithId,deleteLaunchById}= require('../../models/lunches.model');
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existLaunchWithId,
+  deleteLaunchById,
+} = require("../../models/lunches.model");
 
-function httpGetAllLaunches(req,res){
-    res.status(200).json(getAllLaunches());
+function httpGetAllLaunches(req, res) {
+  res.status(200).json(getAllLaunches());
 }
 
-function httpAddNewLaunch(req,res){
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
 
-    const launch= req.body;
-    launch.launchDate = new Date(launch.launchDate)
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.target ||
+    !launch.launchDate
+  ) {
+    return res.status(400).json({
+      error: "missing required launch property",
+    });
+  }
 
-    if(!launch.launchDate || !launch.mission || !launch.rocket || !launch.target){
-        return res.status(400).json({
-            error:'missing required launch property'
-        })
-    }
+  launch.launchDate = new Date(launch.launchDate);
+  console.log(launch.launchDate);
 
-    if(isNaN(launch.launchDate)){
-        res.status(400).json({
-            error: 'invalid date format'
-        })
-    }
+  if(isNaN(launch.launchDate)){
+      res.status(400).json({
+          error: 'invalid date format'
+      })
+  }
 
-    addNewLaunch(launch);
-    res.status(201).json(launch);
+  addNewLaunch(launch);
+  res.status(201).json(launch);
 }
 
-function httpDeleteLaunch(req,res){
-    // console.log(req);
-    const launchId= Number(req.params.id);    
-    console.log(launchId);
-    if(!existLaunchWithId(launchId)){
-        return res.status(404).json({
-            error: 'launch not found'
-        })
-    }
+function httpDeleteLaunch(req, res) {
+  // console.log(req);
+  const launchId = Number(req.params.id);
+  console.log(launchId);
+  if (!existLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: "launch not found",
+    });
+  }
 
-    const aborted= deleteLaunchById(launchId);
-    res.status(200).json(aborted);
-
+  const aborted = deleteLaunchById(launchId);
+  res.status(200).json(aborted);
 }
 
-
-
-module.exports={
-    httpGetAllLaunches,
-    httpAddNewLaunch,
-    httpDeleteLaunch,
-    
-}
+module.exports = {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+  httpDeleteLaunch,
+};
