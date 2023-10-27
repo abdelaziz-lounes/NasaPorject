4c22479d5c7d44709e51285d2c3b35437078b5fe
abdelaziz_lounes <abdelazizlounes2@gmail.com>
2023-10-27 20:58:38 +0100

    connecting to mongoDb

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,18 @@
 const http = require("http");
 const app = require("./app");
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 const MONGO_URL =
   "mongodb+srv://nasa-api:lhYB2HVmoA4yFzsT@nasacluster.1f6qmm7.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connection.once('open',()=>{
+  console.log('mongodb connection is ready!');
+})
+mongoose.connection.on('error',(error)=>{
+  console.error(error);
+})
 
 const server = http.createServer(app);
 
