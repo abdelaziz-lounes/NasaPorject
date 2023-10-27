26b4ba83c15f4491fb40868de0ff995aa26a8443
abdelaziz_lounes <abdelazizlounes2@gmail.com>
2023-10-27 20:38:13 +0100

    installing mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,19 @@
 const http = require("http");
 const app = require("./app");
+const mongoose= require('mongoose');
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://nasa-api:lhYB2HVmoA4yFzsT@nasacluster.1f6qmm7.mongodb.net/?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
-    console.log(`litening on port ${PORT} ...`);
+    console.log(`listening on port ${PORT} ...`);
   });
 }
 
